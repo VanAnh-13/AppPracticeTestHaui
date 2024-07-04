1e51684ebda4fc1f1863e731132f214cb0e21dcf
Duckhanhh <Duckhanhh>
2024-07-04 22:58:28 +0700

    fix login :)

--- app/src/main/java/com/example/nonameapp/ui/LoginFragment.kt
+++ app/src/main/java/com/example/nonameapp/ui/LoginFragment.kt
@@ -1,15 +1,18 @@
 package com.example.nonameapp.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.example.nonameapp.R
 import com.example.nonameapp.base.BaseFragment
 import com.example.nonameapp.base.BaseViewModel
 import com.example.nonameapp.databinding.FragmentLoginBinding
+import retrofit2.Callback
+import com.example.nonameapp.data.source.network.RetrofitClient
+import com.example.nonameapp.model.LoginResponse
+import retrofit2.Call
+import retrofit2.Response
+import android.content.Context
+import com.example.nonameapp.request.LoginRequest
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
     private val myViewModel: BaseViewModel by viewModels()
@@ -31,10 +34,11 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
             val password = binding.passwordEditText.text.toString().trim()
 
             if (validate(email, password)) {
-                // Xử lý logic đăng nhập
-                Toast.makeText(requireContext(), "Login successful", Toast.LENGTH_SHORT).show()
+                performLogin(email, password)
             } else {
-                if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+                        .matches()
+                ) {
                     Toast.makeText(requireContext(), "Invalid email", Toast.LENGTH_SHORT).show()
                 }
                 if (password.isEmpty()) {
@@ -60,5 +64,37 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
             return false
         }
         return true
+    }
+
+    // Logic Đăng nhập
+    private fun performLogin(email: String, password: String) {
+        val call = RetrofitClient.apiService.login(loginRequest = LoginRequest(email, password))
+
+        call.enqueue(object : Callback<LoginResponse> {
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                if (response.isSuccessful) {
+                    val loginResponse = response.body()
+                    if (loginResponse != null) {
+                        saveAccessToken(loginResponse.data.accessToken)
+                    }
+                    Toast.makeText(requireContext(), response.message(), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                Toast.makeText(requireContext(), "Login failed: ${t.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
+    }
+
+    private fun saveAccessToken(accessToken: String) {
+        val sharedPreferences =
+            requireContext().getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putString("access_token", accessToken)
+            apply()
+        }
+        Toast.makeText(requireContext(), "Đăng nhập thành công", Toast.LENGTH_SHORT).show()
     }
 }
